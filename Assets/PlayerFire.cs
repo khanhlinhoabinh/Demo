2358dd8ceac65214c27db7ea8c7453268064fb1e
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2026-01-31 23:27:52 +0700

    Demo 3: enemy, bullet, explosion

---
+++ Assets/PlayerFire.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float shootingInterval = 0.2f;
+
+    // 👉 OFFSET BẮN ĐẠN (CHỈNH TRONG INSPECTOR)
+    public Vector3 bulletOffset;
+
+    private float lastBulletTime;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time - lastBulletTime > shootingInterval)
+            {
+                ShootBullet();
+                lastBulletTime = Time.time;
+            }
+        }
+    }
+
+    void ShootBullet()
+    {
+        Instantiate(
+            bulletPrefab,
+            transform.position + bulletOffset,
+            transform.rotation
+        );
+    }
+}
